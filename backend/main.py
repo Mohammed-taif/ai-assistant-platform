f7999c0de9a127331e995637329e98f2fee7cf9e
Mohammed taif <taif4226@gmail.com>
2026-06-02 11:54:29 +0530

    Integrate Groq AI chatbot

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from dotenv import load_dotenv
+from groq import Groq
+import os
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
 
 app = FastAPI()
 
@@ -8,21 +17,28 @@ class ChatRequest(BaseModel):
 
 @app.get("/")
 def root():
-    return {"message": "AI Assistant Backend Running"}
+    return {
+        "message": "AI Assistant Backend Running"
+    }
 
 @app.post("/chat")
 def chat(request: ChatRequest):
+    try:
+        completion = client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=[
+                {
+                    "role": "user",
+                    "content": request.message
+                }
+            ]
+        )
 
-    msg = request.message.lower()
+        return {
+            "reply": completion.choices[0].message.content
+        }
 
-    if "hello" in msg:
-        return {"reply": "Hello! How can I help you today?"}
-
-    elif "bye" in msg:
-        return {"reply": "Goodbye! Have a great day."}
-
-    elif "how are you" in msg:
-        return {"reply": "I'm doing well, thank you for asking."}
-
-    else:
-        return {"reply": "I'm still learning. Can you rephrase that?"}
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
